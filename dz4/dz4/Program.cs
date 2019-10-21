7ae3fb56577ddad24940f7fa8aad9113d8828201
lamamalaga <tomchuk.01@gmail.com>
2019-10-21 16:34:57 +0500

    готовая программа

---
+++ dz4/dz4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dz4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите символ");
+            char x = char.Parse(Console.ReadLine());
+            int hex = Convert.ToInt32(x);
+            Console.WriteLine("0x{ 0},0x{1}", hex, hex + 1);
+            Console.ReadKey();
+       
+           
+            
+                
+            
+           
+
+        }
+    }
+}
