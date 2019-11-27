443507b7b8486d3578923f6ee5cf50b08aeafb36
lamamalaga <tomchuk.01@gmail.com>
2019-11-27 16:34:41 +0500

    Консультация

---
+++ HOMEWORK/helloWorld/helloWorld/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите коэффициенты уравнения ax^2+bx+c=0 ");
+
+            Console.Write("a = ");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.Write("b = ");
+            var b = double.Parse(Console.ReadLine());
+
+            Console.Write("c = ");
+            var c = double.Parse(Console.ReadLine());
+
+            if(a == 0)
+            {
+                Console.WriteLine("Это не квадратное уравнение (a=0)");
+                    Console.ReadKey();
+                return;
+            }
+
+            else
+            Console.WriteLine("Решаем уравнение ({0})x^2 + ({1})x + ({2}) = 0");
+
+            double x1, x2;
+
+            var d = b * b - 4 * a * c;
+
+            if (d > 0)
+            { var sqrd = Math.Sqrt(d);
+                x1 = (-b - sqrd) / (2 * a);
+                x2 = (-b + sqrd) / (2 * a);
+                Console.WriteLine("Два корня x1={0} и x2={1}", x1, x2);
+            }
+
+            else if (d == 0)
+            {
+                x1 = -b / (2 * a);
+                Console.WriteLine("Один корень", x1);
+
+            }
+            else
+                Console.WriteLine("Корней нет");
+
+
+            Console.ReadKey();
+        }
+    }
+}
